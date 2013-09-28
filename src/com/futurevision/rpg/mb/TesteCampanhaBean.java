05c182ad7466408636a9d93c85b80f330e343ff2
Everton Nascimento <evenascto@gmail.com>
2013-09-28 19:59:25 -0300

    testes das demais classes

--- src/com/futurevision/rpg/mb/TesteCampanhaBean.java
+++ src/com/futurevision/rpg/mb/TesteCampanhaBean.java
@@ -7,9 +7,21 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import com.futurevision.rpg.bo.CampaignBO;
+import com.futurevision.rpg.bo.GameBO;
 import com.futurevision.rpg.bo.inter.CampaignBOI;
+import com.futurevision.rpg.bo.inter.CharacterBOI;
+import com.futurevision.rpg.bo.inter.GameBOI;
+import com.futurevision.rpg.bo.inter.GameUserBOI;
+import com.futurevision.rpg.bo.inter.ItemBOI;
+import com.futurevision.rpg.bo.inter.ScenarioBOI;
 import com.futurevision.rpg.dao.inter.CampaignDAOI;
+import com.futurevision.rpg.entity.Attribute;
 import com.futurevision.rpg.entity.Campaign;
+import com.futurevision.rpg.entity.Character;
+import com.futurevision.rpg.entity.GameUser;
+import com.futurevision.rpg.entity.Item;
+import com.futurevision.rpg.entity.ResultCombat;
+import com.futurevision.rpg.entity.Scenario;
 
 @ManagedBean
 @RequestScoped
@@ -17,38 +29,93 @@ public class TesteCampanhaBean {
 	@EJB 
 	private CampaignBOI campaignBO;
 	
-	private String descricao;
+	@EJB 
+	private CharacterBOI charBO;	
+
+	@EJB 
+	private GameBOI gameBO;	
+	
+	@EJB 
+	private GameUserBOI gameUserBO;	
+
+	@EJB 
+	private ItemBOI itemBO;	
+
+	@EJB 
+	private ScenarioBOI scenarioBO;	
+
+	
+	private String descricaoRetorno;
 
 	public void AgoraVaiBO() throws Exception {
-		/* 
-		 * quando chamei o BO, o entityManager fica nulo. Parece que naõ está reconhecendo como um Bean, ou a injeção de dependencia
-		 * nao funciona nesse caso.
-		 * 
-		 * chamando pelo DAO, funciona.
-		 * ***********************
-		 * 
-		 * descricao = campaignBO.getAllCampaign().get(0).getDescription();
-		 * */
-		
 		//List<Campaign> campanhas = campaignDAO.listAll();
 		List<Campaign> campanhas = campaignBO.getAllCampaign();
 		if(campanhas.size() > 0) 
-			descricao = campanhas.get(0).getDescription();
+			descricaoRetorno = campanhas.get(0).getDescription();
 		else 
-			descricao = "NENHUMA CAMPANHA CADASTRADA";
+			descricaoRetorno = "NENHUMA CAMPANHA CADASTRADA";
 	}
-	/*
-	public void AgoraVaiComDAO() throws Exception {
-		List<Campaign> campanhas = campaignDAO.listAll();
-		if(campanhas.size() > 0) 
-			descricao = campaignDAO.listAll().get(0).getDescription();
-		else 
-			descricao = "NENHUMA CAMPANHA CADASTRADA";
-	}
-	*/
 	
-	public String getDescricao() {
-		return descricao;
+	public void BuscarCharacter() throws Exception {
+		Character c = charBO.searchCharacterById(1);
+		if(c != null) 
+			descricaoRetorno = c.getName();
+		else 
+			descricaoRetorno = "NENHUM CHARACTER COM CODIGO 1 ENCONTRADO";
+	}	
+
+	public void BuscarGameCombate() throws Exception {
+		Character char1 = new Character();
+		Character char2 = new Character();
+		
+		Attribute a1 = new Attribute();
+		a1.setValue(90);
+		
+		Attribute a2 = new Attribute();
+		a2.setValue(30);
+
+		char1.setDexterity(a1);
+		char2.setDexterity(a2);
+		
+		Attribute life = new Attribute();
+		life.setValue(100);
+		char1.setLife(life);
+		char2.setLife(life);
+		
+		ResultCombat combate = gameBO.combat(char1, char2);
+		if(combate != null) 
+			descricaoRetorno = "vencedor: " + combate.getCharWinner().getName();
+		else 
+			descricaoRetorno = "ResultCombat não ENCONTRADO";
+	}	
+	
+	public void BuscarGameUser() throws Exception {
+		GameUser user = gameUserBO.searchGameUserById(1);
+		if(user != null) 
+			descricaoRetorno = user.getName();
+		else 
+			descricaoRetorno = "NENHUM GAMEUSER COM CODIGO 1 ENCONTRADO";
+	}
+	
+	public void BuscarItem() throws Exception {
+		Item item = itemBO.searchItemById(1);
+		if(item != null) 
+			descricaoRetorno = item.getName();
+		else 
+			descricaoRetorno = "NENHUM ITEM COM CODIGO 1 ENCONTRADO";
+	}	
+	
+	public void BuscarCenario() throws Exception {
+		Scenario scenario = scenarioBO.getScenarioById(1);
+		if(scenario != null) 
+			descricaoRetorno = scenario.getDescription();
+		else 
+			descricaoRetorno = "NENHUM CENARIO COM CODIGO 1 ENCONTRADO";
+	}	
+
+	
+	public String getDescricaoRetorno() {
+		return descricaoRetorno;
 	}
 
 }
