e8637e1c345b71ac5b1a31ea4d904fd181a89f37
Everton Nascimento <evenascto@gmail.com>
2013-09-28 17:07:34 -0300

    commit inicial - apenas campaign

---
+++ src/com/futurevision/rpg/mb/TesteCampanhaBean.java
@@ -0,0 +1,58 @@
+package com.futurevision.rpg.mb;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import com.futurevision.rpg.bo.CampaignBO;
+import com.futurevision.rpg.bo.inter.CampaignBOI;
+import com.futurevision.rpg.dao.inter.CampaignDAOI;
+import com.futurevision.rpg.entity.Campaign;
+
+@ManagedBean
+@RequestScoped
+public class TesteCampanhaBean {
+
+	private CampaignBOI campaignBO;
+	
+	@EJB 
+	private CampaignDAOI campaignDAO;
+	
+	private String descricao;
+
+	public void AgoraVaiBO() throws Exception {
+		/* 
+		 * quando chamei o BO, o entityManager fica nulo. Parece que naõ está reconhecendo como um Bean, ou a injeção de dependencia
+		 * nao funciona nesse caso.
+		 * 
+		 * chamando pelo DAO, funciona.
+		 * ***********************
+		 * 
+		 * descricao = campaignBO.getAllCampaign().get(0).getDescription();
+		 * */
+		
+		//List<Campaign> campanhas = campaignDAO.listAll();
+		campaignBO = new CampaignBO();
+		List<Campaign> campanhas = campaignBO.getAllCampaign();
+		if(campanhas.size() > 0) 
+			descricao = campaignDAO.listAll().get(0).getDescription();
+		else 
+			descricao = "NENHUMA CAMPANHA CADASTRADA";
+	}
+
+	public void AgoraVaiComDAO() throws Exception {
+		List<Campaign> campanhas = campaignDAO.listAll();
+		if(campanhas.size() > 0) 
+			descricao = campaignDAO.listAll().get(0).getDescription();
+		else 
+			descricao = "NENHUMA CAMPANHA CADASTRADA";
+	}
+
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
